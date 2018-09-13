384958213423e745d8a4b5d373bad0ffec2a7aa3
pooja khatua <pooja.khatua@gmail.com>
2018-09-13 10:22:39 +0530

    Login App code
    Login App code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  isDetailsInValid ="true";
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+
+
+
+onSubmit(userForm){
+  this.isDetailsInValid="false";
+  console.log(userForm);
+
+  console.log(userForm.value);
+  console.log(this.isDetailsInValid);
+
+
+
+ if(!userForm.value.username){
+  userForm.value.username = userForm.value.email.split("@")[0];
+
+  userForm.value.username = userForm.value.username.replace(/[0-9]/g, '')
+ }
+ 
+ if(userForm.value.username){
+  userForm.value.username = userForm.value.username.replace(/[0-9]/g, '')
+
+ }
+
+ console.log("final value:::::",userForm.value)
+
+ 
+
+
+}
 }
